fa6ed4d41232cbced804a9128f0b3d07ad233b59
baileywickham <baileywickham@gmail.com>
2017-11-08 08:06:51 -0800

    init

---
+++ physics.java
@@ -0,0 +1,134 @@
+/**
+ * Created by bailey on 6/14/17.
+ */
+import java.util.*;
+public class physics {
+    public static void main(String[] cheese) {
+        System.out.println("hello, Welcome to our phyics project produced by Bailey wickham and Tom lee!");
+        System.out.println("┈┈╱▔▔▔▔▔╲┈┈\n" +
+                "┈▕╋╋╋╋╋╋╋▏┈\n" +
+                "┈▕╳╳╳╳╳╳╳▏┈\n" +
+                "┈┈╲╳╳╳╳╳╱┈┈\n" +
+                "┈┈┈╲╋╋╋╱┈┈┈\n" +
+                "┈┈┈┈╲▂╱┈┈┈┈\n" +
+                "┈┈┈┈▕▅▏┈┈┈┈\n" +
+                "\n");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a letter to begin, type exit to exit" + "\n");
+        String currentLetter = sc.nextLine();
+        while (currentLetter != "exit") {
+        switch (currentLetter) {
+            case "a":
+                System.out.println("Acceleration: An entity, measured in a unit of distance over a unit of time^2, measuring the change in velocity of an object over a change in time. If an object is experiencing any type of change in velocity it must have an acceleration whether it be positive or negative.");
+                System.out.println("enter a new letter! \n");
+                break;
+            case "b":
+                System.out.println("Benjamin Franklin: Screwed over everyone for centuries to come by saying that in a circuit positive charge moves which makes literally no sense because of how freaking huge protons are. Some of Benjamin Franklin’s other “accomplishments” are writing an unnecessarily long story of how he discovered that he cannot become perfect, and getting shocked by a kite. Who better would be on our infamous $100 bill.\n");
+                System.out.println("enter a new letter! \n");
+                break;
+            case "c":
+                System.out.println(" Coulomb: The unit of electrical charge. It is defined as flow of charge of 1 amp per 1 second. Named after 18th century physicist Charles Coulomb who also did a lot of other important things like creating coulomb's law. I would tell you what that is but I honestly have no idea and as you can probably infer that is why I chose a much simpler term for his letter.\n");
+                System.out.println("enter a new letter! \n");
+                break;
+            case "d":
+                System.out.println("Doppler Effect: As an object that is in motion, in a particular direction, gives off a frequency of any kind(this is known as the source), other objects perceive it as a different frequency dependent on the direction and velocity of the source. \n");
+                System.out.println("enter a new letter! \n");
+                break;
+            case "e":
+                System.out.println("Elastic Collision: A collision in which colliding objects bounce off each other. In these situation, both energy and momentum are conserved. This sounds like the Scholastic book fair in elementary school which was the highlight of each school year. Even though I hated reading the book fair was always a great time as I got to go buy cool pencils and erasers. One time I bought a huge pointer finger and Mrs. Palidichuk took it away because me and my friends were having a pointer fight. \n");
+                System.out.println("enter a new letter! \n");
+                break;
+            case "f":
+                System.out.println("Friction: The opposing force that is created between objects moving in opposite directions while in contact. The amount of friction is dependent a constant of friction known as mui, which varies depending mostly on materials and temperature, and the mass of the objects.\n");
+                System.out.println("enter a new letter! \n");
+                break;
+            case "g":
+                System.out.println("Glider: A object that is light in weight, designed to fly through the air without the use of any motor. Building wooden gliders is a great project to help learn the law of forces on an object while having a super fun time. While it may be time extrusive, it is well worth it.\n");
+                System.out.println("enter a new letter! \n");
+                break;
+            case "h":
+                System.out.println("Half Life: The time it takes for half an object to degrade from alpha decay. This is how we date objects that have been underground for a long time. It also also the name of one of the most popular video games of all time.\n" );
+                System.out.println("enter a new letter! \n");
+                break;
+            case "i":
+                System.out.println("Kilogram: The SI unit for mass; abbreviated as kg. When dealing with physics problems it is imperative that one converts all given masses of kg as it is the common unit in all equations. If not one may be marked down 1 point which really really sucks. ");
+                System.out.println("enter a new letter! \n");
+                break;
+            case "j":
+                System.out.println(" Jam Man: A jam man is a breadboard project which includes an LED screen and speaker. It is designed to play music along with a face displayed on the LED screen that matches the music playing. A Jam man is a great example something from our daily lives that consist of circuits. \n");
+                System.out.println("enter a new letter! \n");
+                break;
+            case "k":
+                System.out.println("Kilogram: The SI unit for mass; abbreviated as kg. When dealing with physics problems it is imperative that one converts all given masses of kg as it is the common unit in all equations. If not one may be marked down 1 point which really really sucks. \n");
+                System.out.println("enter a new letter! \n");
+                break;
+            case "l":
+                System.out.println("Linear Relationships: a relationship where both things increase at a constant rate. Some examples are Force vs Acceleration, Distance vs Time, and Work vs Normal Force(assuming all other values are constant). As one increases, the other increases at a proportional rate, both of which are constant.\n");
+                System.out.println("enter a new letter! \n");
+                break;
+            case "m":
+                System.out.println(" Moving Ramp: An example of a moving ramp would be a right triangular shaped block that is on a wheeled cart. When dealing with moving ramps it is impossible to find the needed force that must be applied to said moving ramp in order to maintain the position of an object on said moving ramp.\n");
+                System.out.println("enter a new letter! \n");
+                break;
+            case "n":
+                System.out.println("Normal Force: a force that is always perpendicular to the force applied. Normally the force exerted on an object by a wall or floor preventing the object from falling. \n");
+                System.out.println("enter a new letter! \n");
+                break;
+            case "o":
+                System.out.println("Ohm’s: The SI unit for resistance; one volt per amp. This comes from ohms law which is p=iv. Why was the ohm sad? Because the coulomb(cool ohm) got all the girls.\n");
+                System.out.println("enter a new letter! \n");
+                break;
+            case "p":
+                System.out.println("Physics: Vaguely, it’s the science regarding the nature of matter and energy. Physics is a great subject to learn at school as it is very easy to adjust difficulty for certain ages. It is also a great class full of fun and insightful experiments especially when taught by a great teacher. <3.\n");
+                System.out.println("enter a new letter! \n");
+                break;
+            case "q":
+                System.out.println("The charge of a particle. This charge is really small for most charges but becomes meaningful because of the amount of particles. Protons and electrons are the only particles that hold charges that we know of at the moment.\n");
+                System.out.println("enter a new letter! \n");
+                break;
+            case "r":
+                System.out.println("Radius: the line between the center of the circle and any point on the outside. This is one of the key defining factors of a circle, the radius is always the same. Some dude in the middle of babylon invented this concept. It has most likely been around forever.");
+                System.out.println("enter a new letter! \n");
+                break;
+            case "s":
+                System.out.println("Slope: Rise over run or rate of change. In regards to physics, the slope is also the derivative of graphs which can be used to deduce wanted values from given values. This is the really boring definition of slope. A more fun one is a part of the side of a hill or mountain, especially as a place for skiing.\n");
+                System.out.println("enter a new letter! \n");
+                break;
+            case "t":
+                System.out.println("Torque: that thing that your car makes that makes it go faster and kind of has something to do with horsepower. There is more of it in a diesel car as well. The equation is f=rfsintheta. Torque is a rotating force but it is still hard to explain.");
+                System.out.println("enter a new letter! \n");
+                break;
+            case "u":
+                System.out.println("U is the symbol for potential energy. This is a characteristic of an object based on where the object is, what its mass is, and the force of gravity. This really is an arbitrary number but it can be used with ke to make assumptions about gravity which is very helpful.");
+                System.out.println("enter a new letter! \n");
+                break;
+            case "v":
+                System.out.println("Velocity: Speed as a vector. Therefore you can have negative while negative speed is impossible. Velocity is defined as the change in position divided by the change in time. \n");
+                System.out.println("enter a new letter! \n");
+                break;
+            case "w":
+                System.out.println("Work: The amount of force used to move an object when the motion in in the same or opposite direction as the force. We had to do a lot of work when running up the stairs but if you carry a book around on your head, no work is done. Also name for a popular song by A$AP ");
+                System.out.println("enter a new letter! \n");
+                break;
+            case "x":
+                System.out.println("X-Ray: high- energy photons; high- frequency, short-wavelength electromagnetic waves. When playing videogames x-ray is a common hack/cheat that allows oneself to see through walls, making opponents visible and giving the cheater an unfair advantage. \n");
+                System.out.println("enter a new letter! \n");
+                break;
+            case "y":
+                System.out.println();
+                System.out.println("enter a new letter! \n");
+                break;
+            case "z":
+                System.out.println("Z, Now I know my ABC’s, next time won't you sing with me.\n");
+                System.out.println("enter a new letter! \n");
+                break;
+            case "exit":
+                System.out.println("thank you for playing... I need at 79 to get an A in this class");
+                break;
+
+            }
+            currentLetter = sc.nextLine();
+        }
+
+    }
+}
